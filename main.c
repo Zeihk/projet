e2a9f69d38a1b9a0301daf97a6f06618e853653d
Zeihk <pitchenator@gmail.fake>
2013-03-11 18:52:20 +0100

    Initialisation

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include<math.h>
+
+
+int main(){
+
+printf("Hello World \n");
+int a;
+a= sqrt(4);
+printf("a=%d \n", a);
+return 0;
+}
---
+++ makefile
@@ -0,0 +1,6 @@
+myexec : main.o
+	gcc -o myexec main.o
+	@echo : "Création des liens"
+main.o : main.c
+	gcc -c main.c
+	@echo : "Compilation de main"
