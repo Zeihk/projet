39f90566376d5a03da6a27e3c3db0d2ef7f8a1a9
Zeihk <pitchenator@gmail.fake>
2013-03-13 14:17:54 +0100

    Test de connection

--- main.c
+++ main.c
@@ -5,9 +5,24 @@
 
 int main(){
 
+FILE *fichier=NULL;
+fichier = fopen ("../sons/tele2.wav","r");
+
+if (fichier == NULL)  
+        printf ("Erreur a l'ouverture du fichier\n");  
+    else {  
+        printf ("Fichier bien ouvert !\n");  
+        fclose(fichier);  
+    }  
+
 printf("Hello World \n");
+
 int a;
 a= sqrt(4);
 printf("a=%d \n", a);
 return 0;
 }
+
+
+//Utilisation de fopen pour ouvrir un fichier wav ==>data
+//FILE * f = fopen("wav_file.wav", "r");
--- makefile
+++ makefile
@@ -2,7 +2,7 @@ myexec : main.o
 	gcc -o myexec main.o
 	@echo : "Création des liens"
 main.o : main.c
-	gcc -c main.c
+	gcc -c main.c -lm -lfftw3
 	@echo : "Compilation de main"
 
 # test de connection avec github
